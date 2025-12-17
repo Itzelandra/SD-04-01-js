950b757740ed359556fe09635d2891857a78ad5a
Itzel <itzelrolon2001@gmail.com>
2025-12-17 16:50:00 -0600

    Ejercicios Array

--- src/task5.js
+++ src/task5.js
@@ -2,7 +2,11 @@ const arr0 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
 // Type your code below this line!
 
-
-
+for(let i =0;i < 24; i++){
+    lista = lista + String(i)
+    if (i < numeroUsuario){
+        lista = lista + ", "
+    }
+}
 // Type your code above this line!
 console.log("hola")
